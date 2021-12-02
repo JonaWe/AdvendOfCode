96f993518cdb4314c461c8636c6bc9cfac0b92af
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-02 12:06:28 +0100

    adds day 2 part 1 solution

--- 2021/02/day02.hs
+++ 2021/02/day02.hs
@@ -4,6 +4,7 @@
     Solution created by Jona Wessendorf.
 -}
 
+import Language.Haskell.TH (Dec (ForeignD))
 import System.Environment (getArgs)
 
 main :: IO ()
@@ -15,7 +16,31 @@ run :: String -> IO ()
 run file = do
   input <- readFile file
   let inputList = lines input
-      part1 = input -- TODO implement solution
+      unparsedInstructions = map words inputList
+      instructionList = parseInputList unparsedInstructions
+      Point x y = solution1 instructionList (Point 0 0)
+      part1 = x * y
       part2 = input -- TODO implement solution
   putStrLn $ "Solution Day 2 Part 1: " ++ show part1 ++ "."
   putStrLn $ "Solution Day 2 Part 2: " ++ show part2 ++ "."
+
+data Instruction = Instruction Direction Int
+
+data Direction = Up | Down | Forward
+
+data Point = Point Int Int
+
+parseInputList :: [[String]] -> [Instruction]
+parseInputList ([direction, amount] : tail)
+  | direction == "up" = Instruction Up amountNumber : parseInputList tail
+  | direction == "down" = Instruction Down amountNumber : parseInputList tail
+  | direction == "forward" = Instruction Forward amountNumber : parseInputList tail
+  where
+    amountNumber = read amount
+parseInputList _ = []
+
+solution1 :: [Instruction] -> Point -> Point
+solution1 [] direction = direction
+solution1 (Instruction Up amount : tail) (Point x y) = solution1 tail (Point x (y - amount))
+solution1 (Instruction Down amount : tail) (Point x y) = solution1 tail (Point x (y + amount))
+solution1 (Instruction Forward amount : tail) (Point x y) = solution1 tail (Point (x + amount) y)
