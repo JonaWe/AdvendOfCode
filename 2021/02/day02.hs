decd711cf4a24c6b9d552097037b9d54f490d2f9
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-02 12:29:28 +0100

    adds a solution to day 2 part 2

--- 2021/02/day02.hs
+++ 2021/02/day02.hs
@@ -19,8 +19,9 @@ run file = do
       unparsedInstructions = map words inputList
       instructionList = parseInputList unparsedInstructions
       Point x y = solution1 instructionList (Point 0 0)
+      PointAim x2 y2 _ = solution2 instructionList (PointAim 0 0 0)
       part1 = x * y
-      part2 = input -- TODO implement solution
+      part2 = x2 * y2
   putStrLn $ "Solution Day 2 Part 1: " ++ show part1 ++ "."
   putStrLn $ "Solution Day 2 Part 2: " ++ show part2 ++ "."
 
@@ -29,6 +30,8 @@ data Instruction = Instruction Direction Int
 data Direction = Up | Down | Forward
 
 data Point = Point Int Int
+
+data PointAim = PointAim Int Int Int
 
 parseInputList :: [[String]] -> [Instruction]
 parseInputList ([direction, amount] : tail)
@@ -44,3 +47,9 @@ solution1 [] direction = direction
 solution1 (Instruction Up amount : tail) (Point x y) = solution1 tail (Point x (y - amount))
 solution1 (Instruction Down amount : tail) (Point x y) = solution1 tail (Point x (y + amount))
 solution1 (Instruction Forward amount : tail) (Point x y) = solution1 tail (Point (x + amount) y)
+
+solution2 :: [Instruction] -> PointAim -> PointAim
+solution2 [] pointAim = pointAim
+solution2 (Instruction Up amount : tail) (PointAim x y aim) = solution2 tail (PointAim x y (aim - amount))
+solution2 (Instruction Down amount : tail) (PointAim x y aim) = solution2 tail (PointAim x y (aim + amount))
+solution2 (Instruction Forward amount : tail) (PointAim x y aim) = solution2 tail (PointAim (x + amount) (y + aim * amount) aim)
