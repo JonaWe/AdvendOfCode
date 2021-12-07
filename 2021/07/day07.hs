91151bca9861485d82329a7cc726f4b7f8d3fdf6
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-07 11:12:21 +0100

    adds solution to part 1 of day 7

--- 2021/07/day07.hs
+++ 2021/07/day07.hs
@@ -15,7 +15,50 @@ run :: String -> IO ()
 run file = do
   input <- readFile file
   let inputList = lines input
-      part1 = input -- TODO implement solution
-      part2 = input -- TODO implement solution
+      parsedInput :: [Int]
+      parsedInput = map read (split ',' (head inputList) [])
+      part1 = solution1 parsedInput
+      part2 = 0
   putStrLn $ "Solution Day 7 Part 1: " ++ show part1 ++ "."
   putStrLn $ "Solution Day 7 Part 2: " ++ show part2 ++ "."
+
+--
+-- PART 1
+--
+
+solution1 :: [Int] -> Int
+solution1 input = distance
+  where
+    minAltitude = minimum input
+    maxAltitude = maximum input
+    allAltitudes = getAllAltitudes input minAltitude maxAltitude
+    (bestAltitude, distance) = getBestAltitude allAltitudes (minAltitude, maxAltitude * length input)
+
+getAllAltitudes :: [Int] -> Int -> Int -> [(Int, Int)]
+getAllAltitudes positions min max
+  | max >= min = (max, distanceToNumber positions max 0) : getAllAltitudes positions min (max - 1)
+  | otherwise = []
+
+getBestAltitude :: [(Int, Int)] -> (Int, Int) -> (Int, Int)
+getBestAltitude [] min = min
+getBestAltitude ((alt, dis) : tail) (mAlt, mDis)
+  | dis < mDis = getBestAltitude tail (alt, dis)
+  | otherwise = getBestAltitude tail (mAlt, mDis)
+
+distanceToNumber :: [Int] -> Int -> Int -> Int
+distanceToNumber [] _ distance = distance
+distanceToNumber (x : xs) avg distance = distanceToNumber xs avg $ distance + abs (avg - x)
+
+--
+-- HELPERS
+--
+
+--average :: (Real a, Fractional b) => [a] -> b
+--average xs = realToFrac (sum xs) / fromIntegral (length xs)
+
+split :: Eq a => a -> [a] -> [a] -> [[a]]
+split _ [] current = [current]
+split x (y : ys) current
+  | x == y && null current = split x ys []
+  | x == y = current : split x ys []
+  | otherwise = split x ys $ current ++ [y]
