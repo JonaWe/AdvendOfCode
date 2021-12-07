6d0bb616ef8790636f5d7d93a2600d82cea09d04
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-07 10:04:17 +0100

    adds solution to day 06 part 2

--- 2021/06/day06.hs
+++ 2021/06/day06.hs
@@ -17,8 +17,8 @@ run file = do
   let inputList = lines input
       inputNumbers :: [LanternFish]
       inputNumbers = map read $ split ',' (head inputList) []
-      part1 = solution1 inputNumbers 80
-      part2 = 0
+      part1 = solve inputNumbers 80
+      part2 = solve inputNumbers 256
   putStrLn $ "Solution Day 6 Part 1: " ++ show part1 ++ "."
   putStrLn $ "Solution Day 6 Part 2: " ++ show part2 ++ "."
 
@@ -28,8 +28,39 @@ run file = do
 
 type LanternFish = Int
 
+type Population = [Int]
+
 --
--- PART 1
+-- PART 2
+--
+
+solve :: [LanternFish] -> Int -> Int
+solve fishes days = sum $ simulateDays initialPopulation days
+  where
+    initialPopulation = readPopulation fishes $ replicate 9 0
+
+readPopulation :: [LanternFish] -> Population -> Population
+readPopulation [] pop = pop
+readPopulation (fish : rem) [day0, day1, day2, day3, day4, day5, day6, day7, day8]
+  | fish == 0 = readPopulation rem [day0 + 1, day1, day2, day3, day4, day5, day6, day7, day8]
+  | fish == 1 = readPopulation rem [day0, day1 + 1, day2, day3, day4, day5, day6, day7, day8]
+  | fish == 2 = readPopulation rem [day0, day1, day2 + 1, day3, day4, day5, day6, day7, day8]
+  | fish == 3 = readPopulation rem [day0, day1, day2, day3 + 1, day4, day5, day6, day7, day8]
+  | fish == 4 = readPopulation rem [day0, day1, day2, day3, day4 + 1, day5, day6, day7, day8]
+  | fish == 5 = readPopulation rem [day0, day1, day2, day3, day4, day5 + 1, day6, day7, day8]
+  | fish == 6 = readPopulation rem [day0, day1, day2, day3, day4, day5, day6 + 1, day7, day8]
+  | fish == 7 = readPopulation rem [day0, day1, day2, day3, day4, day5, day6, day7 + 1, day8]
+  | fish == 8 = readPopulation rem [day0, day1, day2, day3, day4, day5, day6, day7, day8 + 1]
+
+simulateDays :: Population -> Int -> Population
+simulateDays pop 0 = pop
+simulateDays pop remainingDays = simulateDays (simulateDay pop) $ remainingDays - 1
+
+simulateDay :: Population -> Population
+simulateDay [day0, day1, day2, day3, day4, day5, day6, day7, day8] = [day1, day2, day3, day4, day5, day6, day7 + day0, day8, day0]
+
+--
+-- OLD SOLUTION FOR PART 1
 --
 
 solution1 :: [LanternFish] -> Int -> Int
