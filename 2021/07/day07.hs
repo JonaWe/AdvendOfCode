c0ff213ca8d6a6da053a32f56368743d8ac2ee52
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-07 11:48:51 +0100

    adds solution to day 7 part 2

--- 2021/07/day07.hs
+++ 2021/07/day07.hs
@@ -18,25 +18,47 @@ run file = do
       parsedInput :: [Int]
       parsedInput = map read (split ',' (head inputList) [])
       part1 = solution1 parsedInput
-      part2 = 0
+      part2 = solution2 parsedInput
   putStrLn $ "Solution Day 7 Part 1: " ++ show part1 ++ "."
   putStrLn $ "Solution Day 7 Part 2: " ++ show part2 ++ "."
+
+--
+-- PART 2
+--
+
+solution2 :: [Int] -> (Int, Int)
+solution2 input = (bestAltitude, distance)
+  where
+    --solution2 input = allAltitudes
+
+    minAltitude = minimum input
+    maxAltitude = maximum input
+    allAltitudes = getAllAltitudes input minAltitude maxAltitude distanceToNumber2
+    (bestAltitude, distance) = getBestAltitude allAltitudes (minAltitude, maxAltitude * length input * length input)
+
+distanceToNumber2 :: [Int] -> Int -> Int -> Int
+distanceToNumber2 [] _ distance = distance
+distanceToNumber2 (x : xs) avg distance = distanceToNumber2 xs avg $ distance + calcDistance (abs (avg - x)) 1
+
+calcDistance :: Int -> Int -> Int
+calcDistance 0 _ = 0
+calcDistance d s = s + calcDistance (d -1) (s + 1)
 
 --
 -- PART 1
 --
 
-solution1 :: [Int] -> Int
-solution1 input = distance
+solution1 :: [Int] -> (Int, Int)
+solution1 input = (bestAltitude, distance)
   where
     minAltitude = minimum input
     maxAltitude = maximum input
-    allAltitudes = getAllAltitudes input minAltitude maxAltitude
+    allAltitudes = getAllAltitudes input minAltitude maxAltitude distanceToNumber
     (bestAltitude, distance) = getBestAltitude allAltitudes (minAltitude, maxAltitude * length input)
 
-getAllAltitudes :: [Int] -> Int -> Int -> [(Int, Int)]
-getAllAltitudes positions min max
-  | max >= min = (max, distanceToNumber positions max 0) : getAllAltitudes positions min (max - 1)
+getAllAltitudes :: [Int] -> Int -> Int -> ([Int] -> Int -> Int -> Int) -> [(Int, Int)]
+getAllAltitudes positions min max calcDistance
+  | max >= min = (max, calcDistance positions max 0) : getAllAltitudes positions min (max - 1) calcDistance
   | otherwise = []
 
 getBestAltitude :: [(Int, Int)] -> (Int, Int) -> (Int, Int)
